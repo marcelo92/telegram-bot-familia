4e182c320422c03e21601282f4207c1d05db58f8
Marcelo Teixeira <marceloat01@gmail.com>
2017-04-14 23:01:02 -0300

    Adicionada framework de bot dp @yagop

--- index.js
+++ index.js
@@ -1,6 +1,90 @@
 const Telegraf = require('telegraf')
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const app = new Telegraf(process.env.BOT_TOKEN)
-app.command('start', (ctx) => ctx.reply('Hey'))
-app.on('sticker', (ctx) => ctx.reply('👍'))
-app.startPolling()
+bot.use(Telegraf.log())
+
+bot.command('onetime', (ctx) => {
+  return ctx.reply('One time keyboard', Markup
+    .keyboard([
+      '/simple',
+      '/inline',
+      '/pyramid'
+    ])
+    .oneTime()
+    .resize()
+    .extra()
+  )
+})
+
+bot.command('custom', (ctx) => {
+  return ctx.reply('Custom buttons keyboard', Markup
+    .keyboard([
+      ['🔍 Search', '😎 Popular'],         // Row1 with 2 button
+      ['☸ Setting', '📞 Feedback'],       // Row2 with 2 button
+      ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 button
+    ])
+    .oneTime()
+    .resize()
+    .extra()
+  )
+})
+
+bot.command('special', (ctx) => {
+  return ctx.reply('Special buttons keyboard', Extra.markup((markup) => {
+    return markup.resize()
+      .keyboard([
+        markup.contactRequestButton('Send contact'),
+        markup.locationRequestButton('Send location')
+      ])
+  }))
+})
+
+bot.command('pyramid', (ctx) => {
+  return ctx.reply('Keyboard wrap', Extra.markup(
+    Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {
+      wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / 2
+    })
+  ))
+})
+
+bot.command('simple', (ctx) => {
+  return ctx.replyWithHTML('<b>Coke</b> or <i>Pepsi?</i>', Extra.markup(
+    Markup.keyboard(['Coke', 'Pepsi'])
+  ))
+})
+
+bot.command('inline', (ctx) => {
+  return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup((m) =>
+    m.inlineKeyboard([
+      m.callbackButton('Coke', 'Coke'),
+      m.callbackButton('Pepsi', 'Pepsi')
+    ])))
+})
+
+bot.command('random', (ctx) => {
+  return ctx.reply('random example',
+    Markup.inlineKeyboard([
+      Markup.callbackButton('Coke', 'Coke'),
+      Markup.callbackButton('Dr Pepper', 'Dr Pepper', Math.random() > 0.5),
+      Markup.callbackButton('Pepsi', 'Pepsi')
+    ]).extra()
+  )
+})
+
+bot.hears(/\/wrap (\d+)/, (ctx) => {
+  return ctx.reply('Keyboard wrap', Extra.markup(
+    Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {
+      columns: parseInt(ctx.match[1])
+    })
+  ))
+})
+
+bot.action('Dr Pepper', (ctx, next) => {
+  return ctx.reply('👍').then(next)
+})
+
+bot.action(/.+/, (ctx) => {
+  return ctx.answerCallbackQuery(`Oh, ${ctx.match[0]}! Great choise`)
+})
+
+bot.startPolling()
