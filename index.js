b03243f0a39e997c3291768db36544c623dc69a2
Marcelo Teixeira <marceloat01@gmail.com>
2017-04-14 22:38:28 -0300

    adicionando arquivos do bot + npm

---
+++ index.js
@@ -0,0 +1,6 @@
+const Telegraf = require('telegraf')
+
+const app = new Telegraf(process.env.BOT_TOKEN)
+app.command('start', (ctx) => ctx.reply('Hey'))
+app.on('sticker', (ctx) => ctx.reply('👍'))
+app.startPolling()
