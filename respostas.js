0bd676e45eee4bc3ee4574c44ca7d1581c6882c7
marcelo <marceloat01@gmail.com>
2017-05-11 00:04:00 -0300

    firebase adicionado

--- respostas.js
+++ respostas.js
@@ -8,3 +8,26 @@ module.exports = {
 	cobrar: "pague_aluguel.jpeg",
 	conta: "Banco do Brasil\nAg: 2447-3\nCc: 46926-2\nCPF: 418.325.328-44"
 };
+
+
+/*
+{
+	moradores:{
+		Montanha: "900",
+		Doug: "900",
+		Alexandre: "1100",
+		Bexiga: "1100"
+	},
+
+	respostas:{
+		bom_dia: "bom dia",
+		conta: "Banco do Brasil\nAg: 2447-3\nCc: 46926-2\nCPF: 418.325.328-44",
+		cobrar: "pague_aluguel.jpeg"
+	}
+
+	lista: {[
+		1:"item1",
+		2:"item2",	
+	]}
+}
+*/
